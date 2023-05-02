604c704d8a931a0098a22d36a544d2dc31b0030d
kudlife_alicloud <ljunyfor@outlook.com>
2023-05-02 21:43:35 +0800

    feat(poll): <poll init commit>

---
+++ poll/server.c
@@ -0,0 +1,187 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/poll.h>
+#include <sys/time.h> // struct timeval
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * sockaddr_in 定义在<netinet/in.h>或<arpa/inet.h>中，
+ * 该结构体解决了sockaddr（定义在<sys/socket.h> 里）的缺陷，
+ * 把port和addr分开存储在两个变量中
+ */
+#include <netinet/in.h>
+#include <arpa/inet.h> // inet_addr
+#include <unistd.h>    //socklen_t
+
+#include <errno.h>
+
+#define MAX_LINE 1024
+#define SERV_ADDR "127.0.0.1"
+#define SERV_PORT 7070
+// 处于 完全连接状态的socket 的上限
+#define BACKLOG 10
+#define MAX_CLIENT 1024
+
+void print_addr_info(int listenfd)
+{
+    struct sockaddr_in listenfd_local_addr;
+    socklen_t listenfd_local_addr_len = sizeof(struct sockaddr_in);
+    int r1 = getsockname(listenfd, (struct sockaddr *)&listenfd_local_addr, &listenfd_local_addr_len);
+    struct sockaddr_in listenfd_remote_addr;
+    socklen_t listenfd_remote_addr_len = sizeof(struct sockaddr_in);
+    int r2 = getpeername(listenfd, (struct sockaddr *)&listenfd_remote_addr, &listenfd_remote_addr_len);
+    printf("fd is: %d\naddress is:%s:%d    remote address is %s:%d\n", listenfd,
+           inet_ntoa(listenfd_local_addr.sin_addr), ntohs(listenfd_local_addr.sin_port),
+           inet_ntoa(listenfd_remote_addr.sin_addr), ntohs(listenfd_remote_addr.sin_port));
+}
+
+void handle_connection(struct pollfd *fds, int max)
+{
+    for (int i = 0; i <= max; i++)
+    {
+        if (fds[i].fd == -1)
+        {
+            continue;
+        }
+        if ((fds[i].revents & POLLIN) == POLLIN)
+        {
+            /**
+             * n_read <= rec_buf.size()
+             * 如果发送的数据大于这个rec_buf，则recv函数多次进入接受缓冲区分批放入该数组
+             * 返回读到的数据长度（可能小于期望长度，因为可能Buf太小，一次读不完）
+             */
+            char buf[MAX_LINE];
+            int connected_fd = fds[i].fd;
+            int n_read = recv(connected_fd, buf, sizeof(buf), 0);
+            if (n_read < MAX_LINE)
+            {
+                buf[n_read] = '\0';
+            }
+            if (n_read > 1)
+            {
+                // 从连接套接字指向的文件中 读出客户端发过来的消息
+                printf("len:%d   client's msg:%s\n", n_read, buf);
+                send(connected_fd, buf, n_read, 0);
+            }
+            else if (n_read == 0)
+            {
+                printf("client disconnect\n");
+                close(connected_fd);
+                fds[i].fd = -1;
+                continue; // ctrl+c 断开客户端
+            }
+            else
+            {
+                perror("read error");
+                close(connected_fd);
+                fds[i].fd = -1;
+                continue;
+            }
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int listenfd;                    // server's listening socket
+    struct sockaddr_in s_addr = {0}; // server's socket address
+    struct sockaddr_in c_addr = {0}; // client's socket address
+    socklen_t c_addrlen = 0;         // client's sockaddr's length
+    int n_ready;
+    int max;
+    struct pollfd fds[MAX_CLIENT];
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenfd < 0)
+    {
+        perror("socket error");
+        return -1;
+    }
+    printf("listenfd = %d\n", listenfd);
+
+    // 地址族（要和socket定义时一样）
+    s_addr.sin_family = AF_INET;
+    // 将 点分十进制字符串表示的ipv4地址 转化为 网络字节序证书表示的ipv4地址
+    s_addr.sin_addr.s_addr = inet_addr(SERV_ADDR);
+    // 将 无符号短整形的主机字节序  转化为  短整形的网络字节序
+    s_addr.sin_port = htons(SERV_PORT);
+
+    if (bind(listenfd, (struct sockaddr *)&s_addr, sizeof(struct sockaddr)) == -1)
+    {
+        perror("bind error");
+        return -1;
+    }
+
+    if (listen(listenfd, BACKLOG) < 0)
+    {
+        perror("listen error");
+        return -1;
+    }
+    printf("server open at %s:%d, with max client=%d(backlog=%d)\n", SERV_ADDR, SERV_PORT, MAX_CLIENT, BACKLOG);
+
+    // initialize
+    for (int i = 0; i < MAX_CLIENT; i++)
+    {
+        // -1 indicates available entry
+        fds[i].fd = -1;
+    }
+
+    // add server fd to pollfds
+    fds[0].fd = listenfd;
+    fds[0].events = POLLIN;
+    fds[0].revents = 0;
+    max = 0;
+    n_ready = -1;
+
+    while (1)
+    {
+        n_ready = poll(fds, max + 1, -1);
+        if (n_ready < 0)
+        {
+            perror("poll error");
+            continue;
+        }
+
+        if ((fds[0].revents & POLLIN) == POLLIN)
+        {
+            int connected_fd = accept(fds[0].fd, (struct sockaddr *)&c_addr, &c_addrlen);
+            if (connected_fd < 0)
+            {
+                perror("accept error");
+                continue;
+            }
+            printf("new client:");
+            print_addr_info(connected_fd);
+            int i;
+            for (i = 1; i < MAX_CLIENT; i++)
+            {
+                if (fds[i].fd == -1)
+                {
+                    fds[i].fd = connected_fd;
+                    fds[i].events = POLLIN;
+                    break;
+                }
+            }
+            if (i >= MAX_CLIENT)
+            {
+                printf("too many clients!");
+                continue;
+            }
+            if (i > max)
+            {
+                max = i;
+            }
+
+            if (--n_ready <= 0)
+            {
+                continue;
+            }
+        }
+
+        handle_connection(fds, max);
+    }
+    return 0;
+}
